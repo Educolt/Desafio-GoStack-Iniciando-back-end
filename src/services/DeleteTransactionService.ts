1cfbb85452b10051f027122debff5a980d7b180d
Educolt <Edupiece@hotmail.com>
2020-04-21 00:08:38 -0300

    Application

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
-class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+export default class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found.');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
-
-export default DeleteTransactionService;
